1260aa7d14a230b689c478be207764e9841baf92
Codeblogger <m.furkangulsen@hotmail.com>
2020-08-09 11:51:00 +0300

    k-means-color

---
+++ K-means Color Clustering/k_means_color.py
@@ -0,0 +1,45 @@
+# import the necessary packages
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import argparse
+import cv2
+import numpy as np
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+ap.add_argument("-c", "--clusters", required = True, type = int,
+	help = "# of clusters")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+image = image.reshape((image.shape[0] * image.shape[1], 3))
+
+def centroid_histogram(clt):
+	numLabels = np.arange(0, len(np.unique(clt.labels_))+1)
+	(hist, _) = np.histogram(clt.labels_, bins = numLabels)
+	hist = hist.astype('float')
+	hist /= hist.sum()
+	return hist
+
+def plot_colors(hist, centroids):
+	bar = np.zeros((50, 300, 3), dtype = "uint8")
+	startX = 0
+	for (percent, color) in zip(hist, centroids):
+		endX = startX + (percent * 300)
+		cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
+			color.astype("uint8").tolist(), -1)
+		startX = endX
+	return bar
+
+
+clt = KMeans(n_clusters = args["clusters"])
+clt.fit(image)
+
+hist = centroid_histogram(clt)
+bar = plot_colors(hist, clt.cluster_centers_)
+plt.figure()
+plt.axis("off")
+plt.imshow(bar)
+plt.show()
