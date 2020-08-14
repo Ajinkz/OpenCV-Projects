f14444882ae26d9192627fcb8dfd907c44829b3a
Codeblogger <m.furkangulsen@hotmail.com>
2020-08-15 00:39:45 +0300

    finding the distance of an object

--- Finding the Distance of an Object/Helpers.py
+++ Finding the Distance of an Object/Helpers.py
@@ -27,3 +27,16 @@ class Helpers:
 		else:
 			raise Exception('The length of the contour must be 2 or 3.')
 		return cnts
+
+	def orders(pts):
+		rect = np.zeros((4, 2), dtype = "float32")
+		s = pts.sum(axis = 1)
+
+		rect[0] = pts[np.argmin(s)]
+		rect[2] = pts[np.argmax(s)]
+
+		diff = np.diff(pts, axis = 1)
+		rect[1] = pts[np.argmin(diff)]
+		rect[3] = pts[np.argmax(diff)]
+
+		return rect
--- Finding the Distance of an Object/distance_for_image.py
+++ Finding the Distance of an Object/distance_for_image.py
@@ -1,4 +1,5 @@
 # import the necessary packages
+from Helpers import *
 import numpy as np
 import argparse
 import cv2
@@ -9,8 +10,35 @@ ap.add_argument("-ds", "--distance_image", help = "image file")
 ap.add_argument("-r", "--radius", type = int,help = "gaussian blur")
 args = vars(ap.parse_args())
 
-img1 = cv2.imread(args['main_image'])
-img2 = cv2.imread(args['distance_image'])
-cv2.imshow("img1", img1)
-cv2.imshow("img2", img2)
+def configure_picture(name):
+	img = cv2.imread(args[name])
+	resize = Helpers.resize(img, width=500)
+	gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
+	gray = cv2.GaussianBlur(gray, (5,5), 0)
+	edged = cv2.Canny(gray, 0, 85)
+
+	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+	closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+
+	(cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+	for c in cnts:
+		box = cv2.minAreaRect(c)
+		box = cv2.boxPoints(box)
+		box = np.array(box, dtype="int")
+		box = Helpers.orders(box)
+		(tl, tr, br, bl) = box
+		cv2.drawContours(resize, [box.astype("int")], -1, (0, 0, 255), 2)
+
+	return resize
+
+
+main_image = configure_picture("main_image")
+distance_image = configure_picture("distance_image")
+# book's width: 12cm
+
+
+ 
+cv2.imshow("main_image", main_image)
+cv2.imshow("distance_image", distance_image)
 cv2.waitKey(0)
