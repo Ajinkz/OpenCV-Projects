5c327b570ac75cdb292b403ad81e32efed49aa3f
Codeblogger <m.furkangulsen@hotmail.com>
2020-08-15 02:03:33 +0300

    finding the distance of an object

--- Finding the Distance of an Object/distance_for_image.py
+++ Finding the Distance of an Object/distance_for_image.py
@@ -1,4 +1,5 @@
 # import the necessary packages
+from scipy.spatial import distance as dist
 from Helpers import *
 import numpy as np
 import argparse
@@ -10,12 +11,16 @@ ap.add_argument("-ds", "--distance_image", help = "image file")
 ap.add_argument("-r", "--radius", type = int,help = "gaussian blur")
 args = vars(ap.parse_args())
 
+def distance(a,b):
+	return int(dist.euclidean(a,b))
+
+
 def configure_picture(name):
 	img = cv2.imread(args[name])
 	resize = Helpers.resize(img, width=500)
 	gray = cv2.cvtColor(resize, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (5,5), 0)
-	edged = cv2.Canny(gray, 0, 85)
+	edged = cv2.Canny(gray, 0, 100)
 
 	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
 	closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
@@ -28,17 +33,19 @@ def configure_picture(name):
 		box = np.array(box, dtype="int")
 		box = Helpers.orders(box)
 		(tl, tr, br, bl) = box
-		cv2.drawContours(resize, [box.astype("int")], -1, (0, 0, 255), 2)
+		cv2.drawContours(resize, [box.astype("int")], -1, (199, 214, 66), 4)
 
-	return resize
+		width = distance(tl, tr)
+		height = distance(tl, bl)
+
+	print("width: ", width/21.25)
+	print("height: ", height/21.25)
+	return resize,width,height
 
 
-main_image = configure_picture("main_image")
-distance_image = configure_picture("distance_image")
-# book's width: 12cm
+main_image, mw, mh = configure_picture("main_image")
 
 
  
 cv2.imshow("main_image", main_image)
-cv2.imshow("distance_image", distance_image)
 cv2.waitKey(0)
