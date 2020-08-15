2b071f2373a62b8c2ce4b26a2e980b58256b6dc0
Codeblogger <m.furkangulsen@hotmail.com>
2020-08-15 12:46:49 +0300

    multi scale

--- Multi-scale Template Matching/multi_scale.py
+++ Multi-scale Template Matching/multi_scale.py
@@ -9,8 +9,6 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-t", "--template", required=True, help="Path to template image")
 ap.add_argument("-i", "--images", required=True,
 	help="Path to images where template will be matched")
-ap.add_argument("-v", "--visualize",
-	help="Flag indicating whether or not to visualize each iteration")
 args = vars(ap.parse_args())
 
 template = cv2.imread(args["template"])
@@ -32,18 +30,12 @@ for imagePath in glob.glob(args["images"] + "/*.jpg"):
 		edged = cv2.Canny(resized, 50, 200)
 		result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
 		(_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
-		if args.get("visualize", False):
-			clone = np.dstack([edged, edged, edged])
-			cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
-				(maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
-			cv2.imshow("Visualize", clone)
-			cv2.waitKey(0)
 		if found is None or maxVal > found[0]:
 			found = (maxVal, maxLoc, r)
 	(_, maxLoc, r) = found
 	(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
 	(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
 	
-	cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+	cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 4)
 	cv2.imshow("Image", image)
 	cv2.waitKey(0)
