62c66b2a8bbdda8cb45c00e3fc5de5b88ee0d149
Codeblogger <m.furkangulsen@hotmail.com>
2020-08-22 21:57:47 +0300

    blur detection

--- Blur Detection/blur_detection.py
+++ Blur Detection/blur_detection.py
@@ -1,26 +1,26 @@
-# import the necessary packages
 import cv2
 import argparse
+import glob
+
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--image', required=True,
+ap.add_argument('-i', '--images', required=True,
 	help="path to input directory of images")
 ap.add_argument('-t', '--threshold', type=float,
 	default=float, help="boundary of blurred")
 args = vars(ap.parse_args())
 
-def calculat_laplacian(img):
-	return cv2.Laplacian(img, cv2.CV_64F).var()
+images = [cv2.imread(file) for file in glob.glob("{}/*.jpeg".format(args['images']))]
 
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-fm = calculat_laplacian(gray)
-text = "Not Blurry"
+for image in images:
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	fm = cv2.Laplacian(gray, cv2.CV_64F).var()
+	text = "Not Blurry"
 
-if fm < args["threshold"]:
-	text = "Blurry"
+	if fm < args["threshold"]:
+		text = "Blurry"
 
-cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
-	cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+	cv2.putText(image, "{}: {:.2f}".format(text, fm), (10, 30),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 3)
+	cv2.imshow("Image", image)
+	cv2.waitKey(0)
