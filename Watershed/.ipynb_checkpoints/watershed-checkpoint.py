7264f800527de7df4b386b4899dbc97a8479f466
Codeblogger <m.furkangulsen@hotmail.com>
2020-08-25 03:16:36 +0300

    watershed

--- Watershed/.ipynb_checkpoints/watershed-checkpoint.py
+++ Watershed/.ipynb_checkpoints/watershed-checkpoint.py
@@ -1,8 +1,8 @@
 # import the necessary packages
-from scipy import ndimage
-from skimage.morphology import watershed
+from __future__ import print_function
 from skimage.feature import peak_local_max
-import numpy as np
+from skimage.morphology import watershed
+from scipy import ndimage
 from Helpers import *
 import argparse
 import cv2
@@ -23,24 +23,18 @@ thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 cv2.imshow("Thresh", thresh)
 cv2.waitKey(0)
 
+
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = Helpers.grab_contours(cnts)
+print("[INFO] {} unique contours found".format(len(cnts)))
+
+
 D = ndimage.distance_transform_edt(thresh)
-localMax = peak_local_max(D, indices=False, min_distance=30, labels=thresh)
+localMax = peak_local_max(D, indices=False, min_distance=20, labels=thresh)
 markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
 labels = watershed(-D, markers, mask=thresh)
 print("[INFO] {} unique segments found".format(len(np.unique(labels)) - 1))
 
-for label in np.unique(labels):
-	if label == 0:
-		continue
-	mask = np.zeros(gray.shape, dtype="uint8")
-	mask[labels == label] = 255
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-	c = max(cnts, key=cv2.contourArea)
-	((x, y), r) = cv2.minEnclosingCircle(c)
-	cv2.circle(image, (int(x), int(y)), int(r), (0, 255, 0), 6)
-	cv2.putText(image, "#{}".format(label), (int(x) - 10, int(y)),
-		cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-    
-
-cv2.imshow("Output", image)
-cv2.waitKey(0)
+for i in D:
+	print("i: ", i)
